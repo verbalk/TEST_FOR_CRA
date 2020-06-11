4ba69ab49ff9dd9f4298cb53a703a2ddb6415e9f
verbalk <verbalk89@naver.com>
2020-06-11 15:11:21 +0900

    master

---
+++ src/a.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function A() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload2 edit.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default A;
